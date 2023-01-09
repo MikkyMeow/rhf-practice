4fc2f4235d50d12bf4a76ff3effa2e73adab17f4
Artur Akhmadullin <arturakk2015@gmail.com>
2023-01-09 16:08:04 +0400

    added basic validation

--- src/components/Step1/index.js
+++ src/components/Step1/index.js
@@ -4,10 +4,29 @@ import { Form } from "../Form";
 import { Input } from "../Input";
 import { useForm } from "react-hook-form";
 import { Button } from "../Button";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  firstName: yup
+    .string()
+    .matches(/^([^0-9]*)$/, "First name shouldn't contain numbers")
+    .required("First name is a required field"),
+  lastName: yup
+    .string()
+    .matches(/^([^0-9]*)$/, "Last name shouldn't contain numbers")
+    .required("Last name is a required field"),
+});
 
 export const Step1 = () => {
-  const formMethods = useForm({ mode: "onBlur" });
-  const { handleSubmit, control } = formMethods;
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = (data) => {
     console.log(data);
@@ -19,9 +38,21 @@ export const Step1 = () => {
         🐎 Step 1
       </Typography>
       <Form onSubmit={handleSubmit(onSubmit)}>
-        <Input control={control} label="First Name" name="firstName" />
-        <Input control={control} label="Last Name" name="lastName" />
-        <Button>sef</Button>
+        <Input
+          control={control}
+          label="First Name"
+          name="firstName"
+          error={!!errors.firstName}
+          helperText={errors?.firstName?.message}
+        />
+        <Input
+          control={control}
+          label="Last Name"
+          name="lastName"
+          error={!!errors.lastName}
+          helperText={errors?.lastName?.message}
+        />
+        <Button>Submit</Button>
       </Form>
     </MainContainer>
   );
